4f706deb75f5b4868ea7aab8399451d1b6b95380
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-04-29 20:43:41 +0200

    use Cliente

--- ej1/Cliente/Program.cs
+++ ej1/Cliente/Program.cs
@@ -24,51 +24,27 @@ namespace Client
 
             try
             {
-                Client.Connect("127.0.0.1",10001);
+                Client.Connect("127.0.0.1",10002);
 
                 if (Client.Connected)
                 {
                     Console.WriteLine ("Cliente: Cliente conectado");
+                    FlujoDatos = Client.GetStream();
+
+                    Console.WriteLine("Cliente: Escribe un mensaje para enviar al servidor:");
+                    NetworkStreamClass.EscribirMensajeNetworkStream(FlujoDatos, Console.ReadLine());
+                    Console.WriteLine("Cliente: Mensaje enviado.");
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine ("Error al conectar con el servidor: {0}", e.Message);
             }
-            // try
-            // {
-            //     Client.Connect("127.0.0.1",10001);
-            //     if (Client.Connected)
-            //     {
-            //         Console.WriteLine ("Cliente: Cliente conectado");
-
-            //         FlujoDatos = Client.GetStream();
-            //         //Aquí añadiríamos el Handshake
-
-            //         //Transmisión de datos
-            //         string mensaje ="";
-            //         do
-            //         {
-            //             if (FlujoDatos.CanWrite)
-            //             {
-            //                 mensaje = Console.ReadLine();
-            //                 m.Text = mensaje;
-
-            //                 EscribirMensajeNetworkStream(m);
-
-            //                 FlujoDatos.Write(MensajeBytes,0,MensajeBytes.Length);*/
-            //                 m = LeerMensajeNetWorkStream();
-            //                 Console.WriteLine (m.Text);
-            //             }
-            //         }while (mensaje !="quit");
-            //     }
-            // }
-            // catch (Exception e)
-            // {
-            //     Console.WriteLine ("Error al conectar con el servidor: {0}", e.Message);
-            // }
-
-            Console.ReadLine();
+            finally
+            {
+                FlujoDatos?.Close();
+                Client?.Close();
+            }
         }
 
     }
