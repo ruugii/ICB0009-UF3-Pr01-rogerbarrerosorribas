ebb7cbdb05b19ab91b0e08597c6dcf306c22f955
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-04-29 19:55:44 +0200

    init Client and conect to server

--- ej1/Cliente/Program.cs
+++ ej1/Cliente/Program.cs
@@ -13,9 +13,62 @@ namespace Client
     class Program
     {
 
+        static TcpClient Client;
+        static NetworkStream FlujoDatos;
+
         static void Main(string[] args)
         {
+            byte[] bufferLectura = new byte[1024];
 
+            Client = new TcpClient();
+
+            try
+            {
+                Client.Connect("127.0.0.1",10001);
+
+                if (Client.Connected)
+                {
+                    Console.WriteLine ("Cliente: Cliente conectado");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine ("Error al conectar con el servidor: {0}", e.Message);
+            }
+            // try
+            // {
+            //     Client.Connect("127.0.0.1",10001);
+            //     if (Client.Connected)
+            //     {
+            //         Console.WriteLine ("Cliente: Cliente conectado");
+
+            //         FlujoDatos = Client.GetStream();
+            //         //Aquí añadiríamos el Handshake
+
+            //         //Transmisión de datos
+            //         string mensaje ="";
+            //         do
+            //         {
+            //             if (FlujoDatos.CanWrite)
+            //             {
+            //                 mensaje = Console.ReadLine();
+            //                 m.Text = mensaje;
+
+            //                 EscribirMensajeNetworkStream(m);
+
+            //                 FlujoDatos.Write(MensajeBytes,0,MensajeBytes.Length);*/
+            //                 m = LeerMensajeNetWorkStream();
+            //                 Console.WriteLine (m.Text);
+            //             }
+            //         }while (mensaje !="quit");
+            //     }
+            // }
+            // catch (Exception e)
+            // {
+            //     Console.WriteLine ("Error al conectar con el servidor: {0}", e.Message);
+            // }
+
+            Console.ReadLine();
         }
 
     }
--- ej1/NetworkStreamClass/obj/Debug/net8.0/NetworkStreamClass.AssemblyInfo.cs
+++ ej1/NetworkStreamClass/obj/Debug/net8.0/NetworkStreamClass.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetworkStreamClass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+224cae8385695e8474df2d6c241fb794140b8549")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf3eb945aff0b3e7e27a8def5b3930d8765d8195")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetworkStreamClass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetworkStreamClass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
