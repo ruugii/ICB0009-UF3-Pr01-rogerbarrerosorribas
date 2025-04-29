07673360075f5cbe5cc702b032c75f2bcaf71e19
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-04-29 20:34:09 +0200

    show network stream

--- ej1/Servidor/Program.cs
+++ ej1/Servidor/Program.cs
@@ -20,7 +20,7 @@ namespace Servidor
         static void Main(string[] args)
         {            
             //Asignar una IP y Puerto al servidor
-            Servidor = new TcpListener(IPAddress.Parse("127.0.0.1"),10001);
+            Servidor = new TcpListener(IPAddress.Parse("127.0.0.1"),10002);
 
             Servidor.Start();
             Console.WriteLine("Servidor: Servidor iniciado");
@@ -39,15 +39,35 @@ namespace Servidor
             TcpClient Cliente = (TcpClient)obj;
             if (Cliente.Connected)
             {
-                //Aquí añadiríamos el Handshake
-                //Transmisión de datos
+                try
+                {
+                    //Transmisión de datos
                 Console.WriteLine("Servidor: Cliente conectado");
                 FlujoDatos = Cliente.GetStream();
+
+                // NetworkStream 
+
+                byte[] buffer = new byte[1024];
+        int bytesLeidos = FlujoDatos.Read(buffer, 0, buffer.Length);
+
+        // Convertir los bytes leídos en texto (suponiendo UTF-8)
+        string mensaje = Encoding.UTF8.GetString(buffer, 0, bytesLeidos);
+
+        Console.WriteLine("Servidor: Mensaje recibido del cliente: " + mensaje);
+
                 Vehiculo vehiculo = new Vehiculo();
                 Random randId = new Random();
                 vehiculo.Id = randId.Next(1,1000);
                 vehiculo.Direccion = randId.Next(0,2) == 0 ? "Norte" : "Sur";
                 Console.WriteLine("Servidor: Vehiculo creado con id {0} y direccion {1}", vehiculo.Id, vehiculo.Direccion);
+
+                Console.WriteLine(Cliente.GetStream().ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error desconocido: {0}", e.Message);
+                }
+                
             }
         }
     }
