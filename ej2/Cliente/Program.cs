e6ab3641b3aac8f410951ff77fb43ed950b168b1
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-04-30 10:50:27 +0200

    create ej2

---
+++ ej2/Cliente/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Threading;
+using NetworkStreamNS;
+using CarreteraClass;
+using VehiculoClass;
+
+namespace Client
+{
+    class Program
+    {
+
+        static TcpClient Client;
+        static NetworkStream FlujoDatos;
+
+        static void Main(string[] args)
+        {
+            byte[] bufferLectura = new byte[1024];
+
+            Client = new TcpClient();
+
+            try
+            {
+                Client.Connect("127.0.0.1",10002);
+
+                if (Client.Connected)
+                {
+                    Console.WriteLine ("Cliente: Cliente conectado");
+                    FlujoDatos = Client.GetStream();
+
+                    NetworkStreamClass.EscribirMensajeNetworkStream(FlujoDatos,"Inicio");
+                    string IdRecibido = NetworkStreamClass.LeerMensajeNetworkStream(FlujoDatos);
+                    NetworkStreamClass.EscribirMensajeNetworkStream(FlujoDatos, IdRecibido);
+                    Console.WriteLine("Cliente: Escribe un mensaje para enviar al servidor:");
+                    string mensaje = Console.ReadLine();
+                    NetworkStreamClass.EscribirMensajeNetworkStream(FlujoDatos, mensaje);
+                    Console.WriteLine("Cliente: Mensaje enviado.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine ("Error al conectar con el servidor: {0}", e.Message);
+            }
+            finally
+            {
+                FlujoDatos?.Close();
+                Client?.Close();
+            }
+        }
+
+    }
+}
---
+++ ej2/Servidor/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Threading;
+using NetworkStreamNS;
+using CarreteraClass;
+using VehiculoClass;
+
+namespace Servidor
+{
+
+        class Program
+    {
+        static TcpListener Servidor;
+        static string HostName = "localhost";
+
+        static List<Vehiculo> vehiculos = new List<Vehiculo>();
+
+        static List<Client> clientes = new List<Client>();
+        static void Main(string[] args)
+        {            
+            //Asignar una IP y Puerto al servidor
+            Servidor = new TcpListener(IPAddress.Parse("127.0.0.1"),10002);
+
+            Servidor.Start();
+            Console.WriteLine("Servidor: Servidor iniciado");
+
+            while (true)
+            {
+                TcpClient Cliente = Servidor.AcceptTcpClient();
+                
+                Thread hilo = new Thread(newVehiculo);
+                hilo.Start(Cliente);
+            }
+        }
+
+        static void newVehiculo(object obj)
+        {
+            TcpClient Cliente = (TcpClient)obj;
+            if (Cliente.Connected)
+            {
+                try
+                {
+                    //Transmisión de datos
+                Console.WriteLine("Servidor: Cliente conectado");
+                NetworkStream FlujoDatos;
+                FlujoDatos = Cliente.GetStream();
+                string msg = NetworkStreamClass.LeerMensajeNetworkStream(FlujoDatos);
+                Vehiculo vehiculo = new Vehiculo();
+                int nextId = 0;
+                Random randId = new Random();
+                if (msg == "Inicio")
+                {
+                    nextId = vehiculos.Count + 1;
+                    NetworkStreamClass.EscribirMensajeNetworkStream(FlujoDatos, nextId.ToString());
+                    Client clienteAux = new Client(nextId, FlujoDatos);
+                    clientes.Add(clienteAux);
+                    Console.WriteLine("Servidor: Numero de clientes {0}", clientes.Count);
+                    string idRecibido = NetworkStreamClass.LeerMensajeNetworkStream(FlujoDatos);
+                    if (int.TryParse(idRecibido, out nextId))
+                    {
+                        vehiculo.Id = nextId;
+                        vehiculo.Direccion = randId.Next(0,2) == 0 ? "Norte" : "Sur";
+                        Console.WriteLine("Servidor: Vehiculo creado con id {0} y direccion {1}", vehiculo.Id, vehiculo.Direccion);
+                        Console.WriteLine(Cliente.GetStream().ToString());
+                        vehiculos.Add(vehiculo);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Servidor: Error al recibir el id del vehiculo");
+                        Console.WriteLine("Servidor: El id recibido es {0}", idRecibido);
+                        Console.WriteLine("Servidor: El id esperado es {0}", nextId);
+                    }
+                }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error desconocido: {0}", e.Message);
+                }
+                
+            }
+        }
+    }
+}
+
