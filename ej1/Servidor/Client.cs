a633b54b1ed3d4df4498887480c7c3f692343a94
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-04-30 09:11:47 +0200

    create class Client

---
+++ ej1/Servidor/Client.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Sockets;
+
+namespace Servidor;
+
+public class Client
+{
+  public int Id { get; set; }
+  public NetworkStream Stream { get; set; }
+
+  public Client(int id, NetworkStream stream)
+  {
+    Id = id;
+    Stream = stream;
+  }
+}
